891aeff702a7b32f01171f3a094c2dac4b2af50b
Deepanshu joshi <151924025+Deepanshu3130@users.noreply.github.com>
2026-05-16 00:28:28 +0530

    Fix BASE_URL for production API endpoint

--- frontend/src/operations/apiConnectors.js
+++ frontend/src/operations/apiConnectors.js
@@ -1,7 +1,7 @@
  import axios from "axios";
 import { useChatStore } from "../store/useChatStore";
 import toast from "react-hot-toast";
- const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api/v1" : "https://skill-pick-server.onrender.com";
+ const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api/v1" : "https://skill-pick-server.onrender.com/api/v1";
 export const getUsers = async(token)=>{
     
     
